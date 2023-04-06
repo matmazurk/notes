d83f5ce9bac5160321622d4e5a6623bd68889149
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-04-06 12:40:59 +0200

    adjust script, change user name of github actions

--- readme.md
+++ readme.md
@@ -1,5 +1,2 @@
 # Notes
 
-## 1.[Golang](topics/golang.md)
-## 2.[Raspberry_pi](topics/raspberry_pi.md)
-## 3.[Gcp](topics/gcp.md)
--- script.py
+++ script.py
@@ -8,7 +8,7 @@ lines = len(readme_lines)
 for topic_file in topics_files:
     topic = topic_file.split('.')[0] 
     if not any(topic in s for s in readme_lines_lower):
-        to_write = "## {}.[{}]({}/{})\n".format(lines - 1, topic.capitalize(), 'topics', topic_file)
+        to_write = "## {}. [{}]({}/{})\n".format(lines - 1, topic.capitalize(), 'topics', topic_file)
         readme.write(to_write)
         lines += 1
 
